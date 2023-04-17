649a3103a5c6ba15b263f0c8b43f8178863c1c37
Kyubiso <jakka.kontakt@gmail.com>
2023-04-17 20:33:34 +0200

    April Update

---
+++ cmake-build-debug/CMakeFiles/Sdizo1.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/kuba/CLionProjects/Sdizo1/List.cpp" "CMakeFiles/Sdizo1.dir/List.cpp.o" "gcc" "CMakeFiles/Sdizo1.dir/List.cpp.o.d"
+  "/home/kuba/CLionProjects/Sdizo1/MaxHeap.cpp" "CMakeFiles/Sdizo1.dir/MaxHeap.cpp.o" "gcc" "CMakeFiles/Sdizo1.dir/MaxHeap.cpp.o.d"
+  "/home/kuba/CLionProjects/Sdizo1/Table.cpp" "CMakeFiles/Sdizo1.dir/Table.cpp.o" "gcc" "CMakeFiles/Sdizo1.dir/Table.cpp.o.d"
+  "/home/kuba/CLionProjects/Sdizo1/main.cpp" "CMakeFiles/Sdizo1.dir/main.cpp.o" "gcc" "CMakeFiles/Sdizo1.dir/main.cpp.o.d"
+  "/home/kuba/CLionProjects/Sdizo1/stack.cpp" "CMakeFiles/Sdizo1.dir/stack.cpp.o" "gcc" "CMakeFiles/Sdizo1.dir/stack.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Sdizo1.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Sdizo1.dir/List.cpp.o"
+  "CMakeFiles/Sdizo1.dir/List.cpp.o.d"
+  "CMakeFiles/Sdizo1.dir/MaxHeap.cpp.o"
+  "CMakeFiles/Sdizo1.dir/MaxHeap.cpp.o.d"
+  "CMakeFiles/Sdizo1.dir/Table.cpp.o"
+  "CMakeFiles/Sdizo1.dir/Table.cpp.o.d"
+  "CMakeFiles/Sdizo1.dir/main.cpp.o"
+  "CMakeFiles/Sdizo1.dir/main.cpp.o.d"
+  "CMakeFiles/Sdizo1.dir/stack.cpp.o"
+  "CMakeFiles/Sdizo1.dir/stack.cpp.o.d"
+  "Sdizo1"
+  "Sdizo1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Sdizo1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
